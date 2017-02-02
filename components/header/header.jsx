d53b3a604737edf37f3be81fab68a9e46a6191f3
Joyce Chau <jyc.chau@gmail.com>
2017-02-02 09:47:58 -0800

    Add styling to header

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -7,75 +7,80 @@ export default class Header extends React.Component{
 
   render() {
     return (
-			<div className="header">
-				<div className="header-title">
-					Search and Sort Visualizer
-				</div>
-				<div className="collaborators">
-					<div className="collaborators-title">
-						Collaborators:
-					</div>
-					<div className="authors">
-						<div className="andrew">
-							<div>Andrew Yueh</div>
-              <div className="links">
-                <a href=""
-                  target="_blank"
-                  className="linkedin-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/v1486007000/linkedin2.jpg"
-                    alt="linkedin"
-                    className="linkedin-image"/>
-                </a>
-                <a href=""
-                  target="_blank"
-                  className="github-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
-                    alt="github"
-                    className="github-image"/>
-                </a>
+      <div>
+        <div className="header-main">
+          <div className="header-title">
+            Search and Sort Visualizer
+          </div>
+          <div className="collaborators">
+            <div className="collaborators-title">
+              Collaborators:
+            </div>
+            <div className="authors">
+              <div className="andrew">
+                <div>Andrew Yueh</div>
+                <div className="links">
+                  <a href=""
+                    target="_blank"
+                    className="linkedin-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/v1486025481/linkedin2.png"
+                      alt="linkedin"
+                      className="linkedin-image"/>
+                  </a>
+                  <a href=""
+                    target="_blank"
+                    className="github-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
+                      alt="github"
+                      className="github-image"/>
+                  </a>
+                </div>
               </div>
-						</div>
-						<div className="jeffrey">
-							<div>Jeffrey Fan</div>
-              <div className="links">
-                <a href=""
-                  target="_blank"
-                  className="linkedin-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/v1486007000/linkedin2.jpg"
-                    alt="linkedin"
-                    className="linkedin-image"/>
-                </a>
-                <a href=""
-                  target="_blank"
-                  className="github-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
-                    alt="github"
-                    className="github-image"/>
-                </a>
+              <div className="jeffrey">
+                <div>Jeffrey Fan</div>
+                <div className="links">
+                  <a href=""
+                    target="_blank"
+                    className="linkedin-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/v1486025481/linkedin2.png"
+                      alt="linkedin"
+                      className="linkedin-image"/>
+                  </a>
+                  <a href=""
+                    target="_blank"
+                    className="github-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
+                      alt="github"
+                      className="github-image"/>
+                  </a>
+                </div>
               </div>
-						</div>
-						<div className="joyce">
-							<div>Joyce Chau</div>
-              <div className="links">
-                <a href=""
-                  target="_blank"
-                  className="linkedin-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/v1486007000/linkedin2.jpg"
-                    alt="linkedin"
-                    className="linkedin-image"/>
-                </a>
-                <a href=""
-                  target="_blank"
-                  className="github-link">
-                  <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
-                    alt="github"
-                    className="github-image"/>
-                </a>
+              <div className="joyce">
+                <div>Joyce Chau</div>
+                <div className="links">
+                  <a href=""
+                    target="_blank"
+                    className="linkedin-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/v1486025481/linkedin2.png"
+                      alt="linkedin"
+                      className="linkedin-image"/>
+                  </a>
+                  <a href=""
+                    target="_blank"
+                    className="github-link">
+                    <img src="https://res.cloudinary.com/joycechau/image/upload/c_scale,w_20/v1485282244/github.png"
+                      alt="github"
+                      className="github-image"/>
+                  </a>
+                </div>
               </div>
-						</div>
-					</div>
-				</div>
-			</div>
+            </div>
+          </div>
+        </div>
+        <div className="header-nav">
+          Testing 
+        </div>
+      </div>
     );
   }
 }
