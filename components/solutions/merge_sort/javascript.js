ca4b6a3dabc3d0b4627d9f71406b11e0dff0f1d8
Joyce Chau <jyc.chau@gmail.com>
2017-02-03 11:00:46 -0800

    Add mergesort solutions

--- components/solutions/merge_sort/javascript.js
+++ components/solutions/merge_sort/javascript.js
@@ -1,1 +1,24 @@
 
+function merge(left, right) {
+  const merged = [];
+
+  while (left.length > 0 && right.length > 0) {
+    let nextItem = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(nextItem);
+  }
+
+  return merged.concat(left, right);
+}
+
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    const middle = Math.floor(array.length / 2);
+
+    const left = mergeSort(array.slice(0, middle));
+    const right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
+  }
+}
