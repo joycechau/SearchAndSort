06664cb6329a8fd1ba42a697af70afbfedacb08b
Jeffrey Fan <jeffreyfan93@gmail.com>
2017-02-04 16:05:22 -0800

    set up merge sort exercise

--- components/merge_sort/merge_sort_exercise.jsx
+++ components/merge_sort/merge_sort_exercise.jsx
@@ -6,11 +6,16 @@ export default class MergeSortExercise extends React.Component {
 
     this.state = {
       array: [1, 2, 3, 4, 5, 6, 7, 8],
-      showTopArray: [2, 5, 1, 6, 8, 3, 4, 7],
-      showBotArray: []
+      showTopArray: [[2,5], [1], [6], [8], [3], [4], [7]],
+      showBotArray: [],
+      iteration: 1,
+      startIdx: 0,
+      endIdx: 2
     };
 
     this.generateNewExercise = this.generateNewExercise.bind(this);
+    this.moveBotToTop = this.moveBotToTop.bind(this);
+    this.moveToBotArray = this.moveToBotArray.bind(this);
   }
 
   shuffle(array) {
@@ -31,7 +36,8 @@ export default class MergeSortExercise extends React.Component {
   generateNewExercise() {
     let showTopArray = this.shuffle(this.state.showTopArray);
     this.setState({
-      showTopArray: showTopArray
+      showTopArray: showTopArray,
+      showBotArray: []
     });
   }
 
@@ -42,22 +48,59 @@ export default class MergeSortExercise extends React.Component {
     });
   }
 
+  moveToBotArray(idx, idx2) {
+    let {
+          showBotArray,
+          showTopArray,
+          iteration,
+          startIdx,
+          endIdx
+        } = this.state;
+debugger
+    let botArray = showBotArray;
+    let num = showTopArray[idx];
+
+    if (idx >= startIdx && idx < endIdx) {
+      if (botArray.includes(num)) {
+        console.log('this num is already in the bot array');
+      } else {
+        console.log('put num down');
+        botArray.push(num);
+
+        iteration += 1;
+        startIdx = endIdx;
+        endIdx = Math.pow(2, iteration);
+
+        this.setState({
+          showBotArray: botArray,
+          iteration: iteration,
+          startIdx: startIdx,
+          endIdx: endIdx
+        });
+      }
+    } else {
+      console.log('not yet');
+    }
+  }
+
   render() {
     return (
       <div>
         <button onClick={this.generateNewExercise}>New Exercise</button>
         <div>The array has already been split, now it's your turn to merge them!</div>
         <div>
-          <span>
-            {this.state.showTopArray.map((num, idx) => (
-              <button key={idx} id={`${idx}`}> {num} </button>
-            ))}
-          </span>
+          {this.state.showTopArray.map((array, idx) => (
+            <span key={idx}>
+              {array.map((num, idx2) => (
+                <button key={idx2} id={`${idx}`} onClick={() => this.moveToBotArray(idx, idx2)}> {num} </button>
+              ))}
+            </span>
+          ))}
         </div>
         <div>
           <span>
-            {this.state.showBotArray.map((num, idx) => (
-              <button key={idx} id={`${idx}`}> {num} </button>
+            {this.state.showBotArray.map((array, idx) => (
+              <button key={idx} id={`${idx}`}> {array} </button>
             ))}
           </span>
         </div>
